18fc26edbd405868f7daa32a678b42268502c734
Tao Chen <taochensyd@gmail.com>
2023-11-17 23:03:45 +1100

    add component prinetr toner level to main page

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import HomartPrinterTonerLevel from '../../components/HomartPrinterTonerLevel/HomartPrinterTonerLevel';
+import HomartPrinterTonerLevel from '../../components/HomartPrinterTonerLevel/HomartPrinterTonerLevel'; // Ensure the path is correct
 import './MainPage.css'; // Import the CSS for MainPage
 
 const MainPage = () => {
     return (
-        <div className="main-container">
+        <div className="main-page">
             <h1>Welcome to Homart Printer Management</h1>
+            <p>This is the home page of your application. Below is the printer toner level:</p>
             <HomartPrinterTonerLevel />
         </div>
     );
